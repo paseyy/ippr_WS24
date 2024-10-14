876a1996dadb02dff40bc4522d3063f5d591a949
Jakob Freiberger <freibja@gmail.com>
2024-10-14 19:34:47 +0200

    first solution for A0

--- A0/interpolation_error.py
+++ A0/interpolation_error.py
@@ -29,13 +29,39 @@ def mssim(
     }
 
     # Implement Eq. (9) from assignment sheet
+
     # S should be an "image" of the SSIM evaluated for a window 
     # centered around the corresponding pixel in the original input image
+    x_pad = np.pad(x, ((r, r), (r, r)), "edge")
+    y_pad = np.pad(y, ((r, r), (r, r)), "edge")
+
+    r_squared = r ** 2
+    n_wave = r_squared / (r_squared - 1)
+
     S = np.ones_like(x)
+    for row in range(S.shape[0]):
+        for col in range(S.shape[1]):
+            patch_x = x_pad[row:row+2*r, col:col+2*r]
+            patch_y = y_pad[row:row+2*r, col:col+2*r]
+
+            mu_x = gaussian_filter(patch_x, **filter_args).mean()
+            mu_y = gaussian_filter(patch_y, **filter_args).mean()
+
+            mu_x_squared = mu_x ** 2
+            mu_y_squared = mu_y ** 2
+
+            sigma_x_squared = n_wave * gaussian_filter((patch_x - mu_x) ** 2, **filter_args).sum()
+            sigma_y_squared = n_wave * gaussian_filter((patch_y - mu_y) ** 2, **filter_args).sum()
+
+            sigma_xy = n_wave * gaussian_filter((patch_x - mu_x) * (patch_y - mu_y), **filter_args).sum()
+
+            S[row, col] = (((2 * mu_x * mu_y + C1) * (2 * sigma_xy + C2)) /
+                           ((mu_x_squared + mu_y_squared + C1) * (sigma_x_squared + sigma_y_squared + C2)))
 
     # crop to remove boundary artifacts, return MSSIM
     pad = (win_size - 1) // 2
     return S[pad:-pad, pad:-pad].mean()
+
 
 
 def psnr(
@@ -43,7 +69,9 @@ def psnr(
     y: np.ndarray,
 ) -> float:
     # Implement Eq. (2) without for loops
-    return 1.
+    mse = np.mean(np.square(x - y))
+
+    return 10 * np.log10(1 / mse)
 
 
 def psnr_for(
@@ -51,7 +79,18 @@ def psnr_for(
     y: np.ndarray,
 ) -> float:
     # Implement Eq. (2) using for loops
-    return 1.
+    num_rows = x.shape[0]
+    num_cols = x.shape[1]
+    ase = 0
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+            diff = x[row, col] - y[row, col]
+            ase += diff * diff
+
+    mse = ase / (num_rows * num_cols)
+
+    return 10 * np.log10(1 / mse)
 
 
 def interpolation_error():
